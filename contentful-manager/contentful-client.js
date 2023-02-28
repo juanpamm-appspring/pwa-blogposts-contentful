43e89c898656e4faf17e8f0164948528b70abcf6
Juan Pablo Moreno Muñoz <moreno.juan@correounivalle.edu.co>
2023-02-28 14:55:32 -0500

    Update contentful-client.js

--- contentful-manager/contentful-client.js
+++ contentful-manager/contentful-client.js
@@ -1,8 +1,8 @@
 const contentful = require('contentful')
 
 const client = contentful.createClient({
-    space: '',
-    accessToken: '',
+    space: 's6avkh58cgtx',
+    accessToken: 'Nkr04qwVCH1k9B0_w0CPmMVkGzFRYYk9lGm3nCFxmv4',
 })
 
 export default client
